0a4ada195cf48d2672d9fe2483b417709d70a582
sammyharrison58 <sammyharrison58@gmail.com>
2026-09-16 20:08:25 +0300

    apply changes

--- here.java
+++ here.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class here {
-    
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("What is your name? ");
+		String name = input.nextLine();
+
+		System.out.println("Hello, " + name + "! Nice to meet you.");
+
+		input.close();
+	}
 }
