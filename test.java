636f6c2b7ce0b3123fdc833fccf6d3c329404bf7
sammyharrison58 <sammyharrison58@gmail.com>
2026-09-18 16:50:35 +0300

    apply changes

---
+++ test.java
@@ -0,0 +1,21 @@
+public class test {
+    public class type {
+        private String name;
+        private int value;
+
+        public type(String name, int value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+    }
+
+}
--- type.java
+++ type.java
@@ -1,3 +1,23 @@
 public class type {
+    private String name;
+    private int value;
+
+    public type(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        type example = new type("Example", 42);
+        System.out.println(example.getName() + ": " + example.getValue());
+    }
 
 }
