e8fb90ff1b2ef7afeb1a8055ac6933961ead0b18
sammyharrison58 <sammyharrison58@gmail.com>
2026-09-21 17:53:02 +0300

    apply changes

---
+++ galtoliters.java
@@ -0,0 +1,8 @@
+public class galtoliters {
+    public static void main(String[] agrs) {
+        double gallons = 10;
+        double liters = gallons * 3.7854;
+        System.out.println("Galan in liters : " + liters);
+    }
+
+}
